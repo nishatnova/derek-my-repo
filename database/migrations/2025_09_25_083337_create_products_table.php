37279bf26821076e7172ec673305c6d1303f6c1f
Nishat Anzum Nova <nishat15-12132@diu.edu.bd>
2025-09-25 15:49:54 +0600

    product table

--- database/migrations/2025_09_25_083337_create_products_table.php
+++ database/migrations/2025_09_25_083337_create_products_table.php
@@ -13,7 +13,26 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('category');
+            $table->string('code')->unique();
+            $table->longText('description');
+            $table->integer('minimum_quantity');
+            $table->decimal('per_price', 10, 2);
+            $table->json('colors')->nullable();
+            $table->json('sizes')->nullable();
+            $table->json('additional_discounts')->nullable();
+            $table->json('photos')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
+
+            $table->index('name');
+            $table->index('category');
+            $table->index('code');
+            $table->index('is_active');
+            $table->index(['category', 'is_active']);
+            $table->index(['name', 'category', 'id']);
+            $table->fullText(['name', 'category', 'code', 'description']);
         });
     }
 
